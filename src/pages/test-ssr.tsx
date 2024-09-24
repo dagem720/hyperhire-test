7d6143d4fff7cffbd044acceaba9f1bb0e484423
Dagmawi Gebremariam <dagem720@gmail.com>
2024-09-24 20:57:04 +0300

    fix: build

--- src/pages/test-ssr.tsx
+++ src/pages/test-ssr.tsx
@@ -1,3 +1,13 @@
+interface PageProps {
+  responseData: SampleData[];
+}
+
+interface SampleData {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+}
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:3000/api/test-ssr');
   const responseData = await response.json(); // Extract JSON data from the response
@@ -8,7 +18,7 @@ export async function getServerSideProps() {
   };
 }
 
-export default function Page({ responseData }) {
+export default function Page({ responseData }: PageProps) {
   return (
     <div>
       <h1>Response</h1>
